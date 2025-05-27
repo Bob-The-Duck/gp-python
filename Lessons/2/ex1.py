00883d03dff58a570ccd6b99914d8a9c7c1f9061
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2025-05-27 23:55:49 +0200

    Lesson 2 - Prepared

---
+++ Lessons/2/ex1.py
@@ -0,0 +1,4 @@
+"""
+Przypomnienie: Jak działa funkcja `print`?
+Stwórz 2 zmienne o różnych typach danych i wypisz je na ekranie.
+"""
---
+++ Lessons/2/ex2.py
@@ -0,0 +1,10 @@
+"""
+Stwórz kilka zmiennych dla różnych typów danych i wypisz je na ekranie.
+Na przykład:
+- imię swojego pupila
+- ilość osób w klasie w szkole
+- czy jesteś spakowany na wycieczkę
+- cena biletu do kina
+Zastanów się nad tym, jakie typy danych są odpowiednie dla tych zmiennych.
+Pamiętaj, aby nazwy zmiennych były zrozumiałe i opisowe.
+"""
