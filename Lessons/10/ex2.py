ad46e6e95420f04269e2b39de5cd0c1ae4dfa664
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2025-07-23 20:09:24 +0200

    Lesson 10 - Completed

--- Lessons/10/ex2.py
+++ Lessons/10/ex2.py
@@ -4,3 +4,12 @@ Funkcja ma przyjmować długości boków, a następnie obliczać i wyświetlać 
 
 Dodatkowo: Funckję obliczająco pole trójkąta prostokątnego.
 """
+
+def pole_prostokata(bok1, bok2):
+    pole = bok1 * bok2
+    print(f"Pole prostokąta {bok1} x {bok2} wynosi {pole}")
+
+a = int(input("Podaj długość pierwszego boku: "))
+b = int(input("Podaj długość drugiego boku: "))
+
+pole_prostokata(a, b)
