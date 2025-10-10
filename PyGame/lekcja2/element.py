84839e5a6a3f13cd0761c18caf535e680b4543a6
Laggy <exploit.hack@hotmail.com>
2025-10-10 16:58:27 +0200

    Nowa lekcja

---
+++ PyGame/lekcja2/element.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Obraz(pygame.sprite.Sprite):
+    def __init__(self,sciezka):
+        super().__init__()
+        self.obraz = pygame.image.load(sciezka)
+
+class Element():
+    def __init__(self,typ):
+        self.wybrany = 0
+        self.lista_obrazow = []
+        for i in range(1,4):
+            sciezka = f'images/{typ}{i}.png'
+            wczytany_obraz = Obraz(sciezka)
+            self.lista_obrazow.append(wczytany_obraz)
+    def wybierzNastepny(self):
+        self.wybrany += 1
+        if self.wybrany > 2:
+            self.wybrany = 0
+    def wybranyObraz(self):
+        return self.lista_obrazow[self.wybrany].obraz
+    
+class NakrycieGlowy(Element):
+    def __init__():
+        super().__init__('head')
+
+class NakrycieGlowy(Element):
+    def __init__():
+        super().__init__('body')
+
+class Oczy(Element):
+    def __init__():
+        super().__init__('eye')
+
+class Bron(Element):
+    def __init__():
+        super().__init__('wepon')
